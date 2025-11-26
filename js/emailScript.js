d94427bfc7f39d807dd977f834c89674017de212
Mitesh <miteshmishra89@gmail.com>
2025-11-26 21:30:14 +0530

    adding link for social media

--- js/emailScript.js
+++ js/emailScript.js
@@ -6,7 +6,7 @@ function sendEmail() {
         message: document.getElementById("message").value
     };
     emailjs.send("service_a5cb7qb", "template_72wwy9a", templateParams)
-        .then(alert("Email sent successfully!"), function(error) {
+        .then(alert("Thank you for contacting GemSphere.ai. We have received your message and will get back to you shortly !"), function(error) {
             console.log("FAILED...", error);
         });
 }
